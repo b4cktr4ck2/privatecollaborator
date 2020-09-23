6c68c1d4feb744d2b9aa9570c64b81e484c2c2a1
Alex Kozlov <alexandre.kozlov@protonmail.com>
2020-09-23 15:48:15 -0400

    No longer need this dnshook script.

--- dnshook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 /usr/local/lib/python3.6/dist-packages/dnslib/fixedresolver.py -r "_acme-challenge.$CERTBOT_DOMAIN. 10 IN TXT \"$CERTBOT_VALIDATION\"" -a $1 &>/dev/null &disown
-sleep 5
